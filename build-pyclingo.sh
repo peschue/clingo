08086bc1097a1fb37aa30c10754ca52dbcc5a6a0
Peter Schueller <schueller.p@gmail.com>
2017-08-14 16:06:19 +0300

    using python3

--- build-pyclingo.sh
+++ build-pyclingo.sh
@@ -1,5 +1,5 @@
 
 mkdir -p build/pyclingo/
-cmake -H./ -B./build/pyclingo/ -DCMAKE_BUILD_TYPE=Release -DCLINGO_BUILD_APPS=OFF -DCLINGO_REQUIRE_PYTHON=ON 
+cmake -H./ -B./build/pyclingo/ -DCMAKE_BUILD_TYPE=Release -DCLINGO_BUILD_APPS=OFF -DCLINGO_REQUIRE_PYTHON=ON -DPYTHON_EXECUTABLE=/usr/bin/python3
 cmake --build build/pyclingo/
 
